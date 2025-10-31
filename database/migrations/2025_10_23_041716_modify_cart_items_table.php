de4a6fa79e20d4264e4afd60d95a4eaf86ddd4a3
tramtt-vn <85148171+tramtt-vn@users.noreply.github.com>
2025-10-31 11:08:11 +0700

    [31102025] update  form search from code, name

--- database/migrations/2025_10_23_041716_modify_cart_items_table.php
+++ database/migrations/2025_10_23_041716_modify_cart_items_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::table('cart_items', function(Blueprint $table){
             $table->integer('quantity')->default(1)->change();
-            $table->timestamps();
         });
     }
 
@@ -22,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::table('cart_items', function (Blueprint $table) {
+            $table->integer('quantity')->default(null)->change();
+        });
     }
 };
